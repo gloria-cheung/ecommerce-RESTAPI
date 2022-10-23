fae4cb0f2d2bbbff75d9b73a6511065cb6fba39d
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-23 14:33:33 -0400

    add register route and use bcrypt to hash password before saving user to db

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,31 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// register
+router.post("/register", async (req, res, next) => {
+  const { username, password, email } = req.body;
+
+  if (!username || !password || !email) {
+    return res.status(400).json("missing credentials");
+  }
+
+  try {
+    //hash password, then create user and save to db
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+// login
 
 module.exports = router;
