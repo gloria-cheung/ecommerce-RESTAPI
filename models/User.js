20b18db4e2378bb917b39a9f82bf4e17478b282c
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-23 15:49:23 -0400

    fix typo to be isAdmin

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmid: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
