df4fb7158102ac3097ef9a9c482c5a3811a5f6db
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 12:53:59 -0400

    add create cart route to make new cart and link to user

--- routes/cart.js
+++ routes/cart.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
+const Cart = require("../models/Cart");
+const { verifyToken, verifyTokenAndAuth } = require("./verifyToken");
+
+//create cart
+router.post("/", verifyToken, async (req, res, next) => {
+  try {
+    const newCart = Cart.create(req.body);
+    res.status(201).json(newCart);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+//update cart
+//delete cart
+//get cart
 
 module.exports = router;
