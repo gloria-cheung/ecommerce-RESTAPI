3c979c7a23bf7d807f838dee4fbac26fe7d93893
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 13:35:38 -0400

    add create new order route

--- routes/order.js
+++ routes/order.js
@@ -1,3 +1,15 @@
 const router = require("express").Router();
+const Order = require("../models/Order");
+const { verifyToken, verifyTokenAndAdmin } = require("./verifyToken");
+
+//create order
+router.post("/", verifyToken, async (req, res, next) => {
+  try {
+    const newOrder = await Order.create(req.body);
+    res.status(201).json(newOrder);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
 module.exports = router;
