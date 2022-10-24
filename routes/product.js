5190717aa24ee8b10f2538f2bcf5fb2be05050e6
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 11:50:00 -0400

    add update product route

--- routes/product.js
+++ routes/product.js
@@ -13,4 +13,26 @@ router.post("/", verifyTokenAndAdmin, async (req, res, next) => {
   }
 });
 
+//update product
+router.put("/:id", verifyTokenAndAdmin, async (req, res, next) => {
+  if (!req.params.id) {
+    return res.status(400).json("missing productID");
+  }
+
+  // set new:true in order to return back the updated version
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
 module.exports = router;
