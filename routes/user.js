d24d30ab47eec7ef3829b930c99145ecd7d498c0
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 09:45:19 -0400

    add update user route that uses verifytokenandauth middleware before find user and update

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,31 @@
 const router = require("express").Router();
+const { verifyToken, verifyTokenAndAuth } = require("./verifyToken");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-//get a user
-router.get("/", async (req, res, next) => {
-  res.json("ok");
+//update user
+router.put("/:id", verifyTokenAndAuth, async (req, res, next) => {
+  try {
+    if (req.body.password) {
+      // if there's password in body, need to hash again before saving
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+
+    // set new:true so that the user is returned from model
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    const { password, ...everythingButPassword } = user._doc;
+    res.status(200).json(everythingButPassword);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 module.exports = router;
