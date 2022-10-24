2298fc8d7b1918ae3e994f8bdea90cbdef63127d
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 09:44:05 -0400

    create func to verify token and another func to verify and authenticate based on whether id matches

---
+++ routes/verifyToken.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  // check if token key is in request header and split since its "Bearer token"
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json("token is invalid");
+      }
+      // set new varibale in req called "user" to be the user that is returned from jwt
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("you are not authenticated");
+  }
+};
+
+const verifyTokenAndAuth = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(403).json("you don't have access");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuth };
