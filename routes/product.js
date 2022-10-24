01e5ffa523d0405afc0fed9ebfd31f0981c2c967
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 11:40:21 -0400

    add create product route for admin to make new product and save to db

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
+const Product = require("../models/Product");
+const { verifyTokenAndAdmin } = require("./verifyToken");
+
+//create product
+router.post("/", verifyTokenAndAdmin, async (req, res, next) => {
+  try {
+    // if await new Product(req.body) => need to call .save()
+    const newProduct = await Product.create(req.body);
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
 module.exports = router;
