9616fd3893aa2dad6aab5633b4bbebe62beb8d90
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 12:10:11 -0400

    add route to get all products based on query new=true and category=x

--- routes/product.js
+++ routes/product.js
@@ -35,4 +35,60 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res, next) => {
   }
 });
 
+//delete product
+router.delete("/:id", verifyTokenAndAdmin, async (req, res, next) => {
+  if (!req.params.id) {
+    return res.status(400).json("missing productID");
+  }
+
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("product deleted");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+//get product
+router.get("/:id", async (req, res, next) => {
+  if (!req.params.id) {
+    return res.status(400).json("missing productID");
+  }
+
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+//get all products
+router.get("/", async (req, res, next) => {
+  // can have 1 query, both or none
+  const queryNew = req.query.new;
+  const queryCategory = req.query.category;
+  let products;
+  
+  try {
+    if (queryNew && queryCategory) {
+      products = await Product.find({
+        categories: { $in: [queryCategory] },
+      })
+        .sort({ createdAt: "desc" })
+        .limit(5);
+    } else if (queryNew) {
+      products = await Product.find().sort({ createdAt: "desc" }).limit(5);
+    } else if (queryCategory) {
+      products = await Product.find({ categories: { $in: [queryCategory] } });
+    } else {
+      products = await Product.find();
+    }
+    
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
 module.exports = router;
