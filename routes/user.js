3d02beda61f5a09763021b6a7be135330c2eb304
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-24 10:16:43 -0400

    add get user route where if token shows isAdmin, can retrieve user from db

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { verifyToken, verifyTokenAndAuth } = require("./verifyToken");
+const { verifyTokenAndAuth, verifyTokenAndAdmin } = require("./verifyToken");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
@@ -33,6 +33,22 @@ router.delete("/:id", verifyTokenAndAuth, async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("user deleted");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+//get user (only admin has access)
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(403).json("user not found");
+    }
+    
+    const { password, ...everythingButPassword } = user._doc;
+    res.status(200).json(everythingButPassword);
   } catch (err) {
     res.status(500).json(err.message);
   }
